749cb0898786a24dbcdd408a15b8ddca1f1a8f13
Gilberto Stankiewicz <kiewic@gmail.com>
2017-06-08 09:51:30 -0700

    Add a default file and lod it if settings do not exist

--- keyboard-shortcuts/js/persistedData.js
+++ keyboard-shortcuts/js/persistedData.js
@@ -1,23 +1,39 @@
 ﻿function PersistedData() {
-    const fileName = 'settings.json';
+    const defaultFileName = 'default.json';
+    const settingsFileName = 'settings.json';
     var localFolder = Windows.Storage.ApplicationData.current.localFolder;
     var replaceExisting = Windows.Storage.CreationCollisionOption.replaceExisting;
     var openIfExists = Windows.Storage.CreationCollisionOption.openIfExists;
     var FileIO = Windows.Storage.FileIO;
 
     function load() {
-        return localFolder.createFileAsync(fileName, openIfExists).then(function (storageFile) {
+        return localFolder.createFileAsync(settingsFileName, openIfExists).then(function (storageFile) {
             return FileIO.readTextAsync(storageFile).then(function (value) {
-                return value;
+                if (value && value.trim() !== "") {
+                    return value;
+                }
+                return loadDefault();
             });
         });
     }
 
     function save(content) {
-        return localFolder.createFileAsync(fileName, replaceExisting).then(function (storageFile) {
+        return localFolder.createFileAsync(settingsFileName, replaceExisting).then(function (storageFile) {
             console.log(storageFile.path);
             return FileIO.writeTextAsync(storageFile, content).then(function () {
                 return;
+            });
+        });
+    }
+
+    function loadDefault() {
+        return Windows.ApplicationModel.Package.current.installedLocation.getFolderAsync('assets').then(function (assetsFolder) {
+            return assetsFolder.getFileAsync(defaultFileName).then(function (storageFile) {
+                return FileIO.readTextAsync(storageFile).then(function (value) {
+                    return value;
+                });
+            }, function (error) {
+                console.error(error);
             });
         });
     }
